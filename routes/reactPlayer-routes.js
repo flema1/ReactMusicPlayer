bb1426d0292c8a674871ccf7b6e1418041a57168
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-11-13 01:10:22 -0500

    added post song route

--- routes/reactPlayer-routes.js
+++ routes/reactPlayer-routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const reactPlayerRouter = express.Router();
 
 const reactPlayerController = require('../controllers/reactPlayer-controller');
+// reactPlayerRouter.get('/', reactPlayerController.index);
+// reactPlayerRouter.get('/songs',reactPlayerController.songs);
+// reactPlayerRouter.get('/test',reactPlayerController.test);
 
-reactPlayerRouter.get('/', reactPlayerController.index);
-reactPlayerRouter.post('/', reactPlayerController.create);
+reactPlayerRouter.post('/song',reactPlayerController.song);
+reactPlayerRouter.post('/search',reactPlayerController.search);
 
-reactPlayerRouter.get('/:id', reactPlayerController.show);
-reactPlayerRouter.put('/:id', reactPlayerController.update);
-reactPlayerRouter.delete('/:id', reactPlayerController.destroy);
 
 module.exports = reactPlayerRouter;
 
