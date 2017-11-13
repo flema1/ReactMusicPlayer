3e88af8f3a72dcc83536334ec94366dfaf694f4f
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-11-13 01:00:32 -0500

    updated App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 import PlayButton from './components/PlayButton';
+import Library from './components/Library';
+
+import Search from './components/Search';
+
+import axios from 'axios';
+import './App.css';
+import * as Ionicons from 'react-icons/lib/io'
+
 
 
 
 class App extends Component {
+  constructor() {
+    super();
+     this.state = {
+         active:false,
+         currentSong:null,
+         streamURl:null,
+         apidataLoaded:false
+        
+    }
+}
   render() {
     return (
       <div className="App">
-        <div >
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<h2>Welcome to React</h2>*/}
+        <Search/>
         </div>
-        <p className="App-intro">
-          {/*<h1>React MusicPlayer</h1>*/}
-        </p>
-        <div className="player_container">
-            <img className="album_cover" src="http://myegy.tv/files/img/content/3/736/1495064508.500_1000.jpg"/>
-           
-          
-             
-               <PlayButton/>
-           
-                        
-        </div>
-      </div>
     );
   }
 }
