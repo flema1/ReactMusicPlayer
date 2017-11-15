040c815b26e2e69172561160c24c47569746692a
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-11-15 03:29:45 -0500

    add icon display

--- client/src/components/BacktaceButton.jsx
+++ client/src/components/BacktaceButton.jsx
@@ -1,55 +1,28 @@
-
-
-
 import React, { Component } from 'react';
 
 
-class BacktaceButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-         playing: false,
+export default class BacktaceButton extends Component {
+    constructor() {
+        super();
+            this.state = {
+                toggle: false
+            }  
+        }
+ 
+    handleClick=()=>{
+        console.log("clicked FF_right")
+        this.setState({toogle: this.state.toogle ? false : true });
     }
-  
-    this.handleClick = this.handleClick.bind(this);
- }
- 
- 
- 
-  handleClick(){
-      console.log("clicked")
-      console.log(this.state.playing)
-       if (this.state.playing===false){
-          this.setState({
-               playing: true
-          });
-        this.audio.play();
-      }
-      else {
-           this.setState({
-               playing: false
-          });
-          this.audio.pause();
-      }   
-  }
 
-   
-            
-
-   render() {
-    return (
-        <div>
-        <div className="f_foward_right">
-                        <div className="b_b_inner">
-                           </div>
-                           <div className="b_b_inner">
-                         </div>
-                    </div>
+    render(){
+        return (
+            <div className="f_foward_right">
+                        <div className="b_b_inner"></div>
+                        <div className="b_b_inner"></div>
             </div>
-    )
-  }
-
+      
+        )
+    }
 }
 
-
-export default BacktaceButton;
+            
--- client/src/components/FastforwardButton.jsx
+++ client/src/components/FastforwardButton.jsx
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
 
 
-class FastforwardButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-         playing: false,
+export default class FastforwardButton extends Component {
+    constructor() {
+        super();
+            this.state = {
+                toggle: false
+            }  
+        }
+ 
+    handleClick=()=>{
+        console.log("clicked FF_left")
+        this.setState({toogle: this.state.toogle ? false : true });
     }
-  
-    this.handleClick = this.handleClick.bind(this);
- }
- 
- 
- 
-  handleClick(){
-      console.log("clicked")
-      console.log(this.state.playing)
-       if (this.state.playing===false){
-          this.setState({
-               playing: true
-          });
-        this.audio.play();
-      }
-      else {
-           this.setState({
-               playing: false
-          });
-          this.audio.pause();
-      }   
-  }
 
-   
-            
-
-   render() {
-    return (
-        <div>
-        <div className="f_foward_left">
-                        <div className="f_foward_inner">
-                           </div>
-                           <div className="f_foward_inner">
-                         </div>
-                    </div>
+    render(){
+        return (
+            <div className="f_foward_left">
+                <div className="f_foward_inner"></div>
+                <div className="f_foward_inner"></div>
             </div>
-    )
-  }
-
+          
+        )
+    }
 }
-
-
-export default FastforwardButton;
