c279546f9e5617c9958dda549bbc64edc2f9d359
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-11-18 02:09:30 -0500

    add redux initial state

---
+++ client/src/redux/initialState.js
@@ -0,0 +1,13 @@
+const initialState = {
+          searchSong:null,
+          loadComponent:false,
+          playlists:null,
+          location:{ x: 0, y: 0 },
+          allPlayLists:null,
+          songsApidataLoaded:false,
+          playListId:null,
+          SinglePlaylistSongs:null,
+          SinglePlaylistSongsApidataLoaded:false
+}
+
+export default initialState
