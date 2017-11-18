29f3debc81fbf69fe80c8515f673593765842316
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-11-18 02:19:27 -0500

    add display playlist component

---
+++ client/src/components/SinglePlaylist.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import SingleSong from './SingleSong';
+import Sound from './Sound';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'; 
+import axios from 'axios';
+
+
+const getPlaylistSongsRedux = (playListId) => {
+  console.log("GET_PLAYLIST_SONGS_DATA");
+  return {
+    type: 'GET_PLAYLIST_SONGS_DATA',
+    playListId:playListId
+  };
+};
+
+
+ class Playlist extends Component {
+  constructor() {
+    super();
+    this.state = {
+         streamURl:null,
+    }
+  }
+
+ componentWillMount() {
+    this.props.loadPlaylistSongs(this.props.playListId);    
+    }
+
+    setSong=(songUrl)=>{
+                this.setState({
+                        streamURl:songUrl
+            })
+        }
+  
+
+ render() {   
+    const {streamURl} = this.state;
+    const { playListId,apidataLoaded, songs} = this.props;
+        return (
+            <div className={'container'} >
+                 <Sound streamURl={streamURl}/>
+                 {apidataLoaded ? songs.map((song, index) => <SingleSong song={song} key={index} setSong={this.setSong}/>):<h1>Search</h1>}
+            </div>
+            )
+    }
+}
+
+
+
+const mapStateToProps = (state) => { 
+  return { 
+         playListId: state.playListId,
+         apidataLoaded:state.SinglePlaylistSongsApidataLoaded,
+         songs:state.SinglePlaylistSongs
+   };
+};
+
+const mapDispatchToProps = (dispatch) =>{
+  return {
+     loadPlaylistSongs(playListId) {
+            dispatch(getPlaylistSongsRedux(playListId));
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
