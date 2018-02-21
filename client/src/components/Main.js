5a7a8749fa741a9160f10a3a0edf86116eb17fc8
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-21 02:39:27 -0500

    update state

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
 import Controls from './Controls';
-import SongInfo from './SongInfo';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import * as Ionicons from 'react-icons/lib/io';
+import Default from './DefaultHome';
 
 
-const setTrackStreamURlRedux = (storeId,trackIndex) => {
-  console.log("herere",storeId);
+const setTrackStreamURlRedux = (storeId, trackIndex) => {
+  console.log("herere", storeId);
   return {
     type: 'SET_Track_STREAM_URL',
-    storeId:storeId,
-    trackIndex:trackIndex
+    storeId: storeId,
+    trackIndex: trackIndex
   };
 };
 
@@ -20,7 +20,7 @@ const getPlaylistSongsRedux = (playListId) => {
   console.log("GET_PLAYLIST_SONGS_DATA");
   return {
     type: 'GET_PLAYLIST_SONGS_DATA',
-    playListId:playListId
+    playListId: playListId
   };
 };
 
@@ -28,83 +28,76 @@ const getPlaylistSongsRedux = (playListId) => {
 const setCurrCover = (coverUrl) => {
   return {
     type: 'SET_CURR_COVER',
-    payload:coverUrl
+    payload: coverUrl
   };
 };
 
 
- class Dashboard extends Component {
+class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-         streamURl:null,
+      streamURl: null,
     }
   }
 
 
-  onHandleClick(song){
-    const { /*song, */index, setSong} = this.props;
+  onHandleClick(song) {
+    const { index,setSongc} = this.props;
     this.props.loadTrackStreamURl(song.storeId, song.index)
     this.props.dispatchSetCurrCover(song.cover[0].url);
   }
   
- render() {   
-    const {streamURl} = this.state;
+  renderCover(){
+    const { currCover } = this.props;
+    return (<div>
+              <img src={currCover}  resizeMode="contain"  className={'mainCover'}  style={{  flex: 1, width: null, height: null}} />
+              <Controls/>
+            </div>)
+  }
+  
+  render() {   
+    const { streamURl } = this.state;
     const { playListId,apidataLoaded, songs, currCover, shift, songsApidataLoaded, songsResults} = this.props;
     console.log(songsApidataLoaded, 'songsApidataLoaded---------------------', songsResults )
-        return (
-             <div 
-                style={{  
+        return (<div 
+                  style={{  
                     backgroundImage: `url(${currCover})`,
                     overflow: 'hidden',
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     left: shift ? '300px': 0, position:'fixed', height:'100%', width: shift? '70%':'100%', backgroundColor:'#4c4b63',
-                    display:'flex', flexDirection:'column', justifyContent:'flex-end', alignItems: 'center', top:'-10px'}}
-            > 
-                {
-                  currCover ?
-                            <div>
-                              <img src={currCover}  resizeMode="contain"  className={'mainCover'}  style={{  flex: 1, width: null, height: null}} />
-                              <Controls/>
-                            </div>:null
-                }
-                {
-                  songsApidataLoaded ? 
-                  <div style={{ flex:1, flexWrap:'wrap', display: 'flex' , overflow:'scroll'}}>
-                   { songsResults.map((song)=><SongInfo song={song} onClick={this.onHandleClick.bind(this, song)}/>) }
-                  </div>
-                  :null
-                }
-        </div>
-            )
+                    display:'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center', top:'-10px'}}
+                > 
+                  { currCover ?  this.renderCover() : <Default redirectTo={ this.props.redirectTo}/> }
+                </div>)
     }
 }
 
 
 
 
-const mapStateToProps = (state) => { 
-  return { 
-         playListId: state.playListId,
-         apidataLoaded:state.SinglePlaylistSongsApidataLoaded,
-         songs:state.SinglePlaylistSongs,
-         currCover:state.currCover,
-         songsApidataLoaded:state.songsApidataLoaded,
-         songsResults: state.songs
-   };
+const mapStateToProps = (state) => {
+  return {
+    playListId: state.main.playListId,
+    apidataLoaded: state.main.SinglePlaylistSongsApidataLoaded,
+    songs: state.main.SinglePlaylistSongs,
+    currCover: state.main.currCover,
+    songsApidataLoaded: state.main.songsApidataLoaded,
+    songsResults: state.main.songs
+  };
 };
 
-const mapDispatchToProps = (dispatch) =>{
+const mapDispatchToProps = (dispatch) => {
   return {
-     loadPlaylistSongs(playListId) {
-            dispatch(getPlaylistSongsRedux(playListId));
+    loadPlaylistSongs(playListId) {
+      dispatch(getPlaylistSongsRedux(playListId));
     },
-       loadTrackStreamURl(storeId,trackIndex) {
-        dispatch(setTrackStreamURlRedux(storeId,trackIndex));
+    loadTrackStreamURl(storeId, trackIndex) {
+      dispatch(setTrackStreamURlRedux(storeId, trackIndex));
     },
-    dispatchSetCurrCover(coverUrl){
-           dispatch(setCurrCover(coverUrl));
+    dispatchSetCurrCover(coverUrl) {
+      dispatch(setCurrCover(coverUrl));
     }
   }
 };
