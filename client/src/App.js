5ddc413392503df1228fa720ab63c62ff66d2310
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-21 02:38:29 -0500

    add artist info scene

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,12 @@ import PlayListMenu from './components/PlayListMenu';
 import SideNav from './partials/SideNav';
 import Login from './components/LoginPage';
 import View from './components/SearchResult';
-import { connect } from 'react-redux';
+import Favorites from './components/Favorites';
+import Library from './components/Library';
+import Artists from './components/Artists.component';
+import ArtistInfo from'./components/ArtistInfo.component';
 import * as Ionicons from 'react-icons/lib/io';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import './App.css';
 
@@ -24,7 +28,6 @@ class App extends Component {
   componentWillMount(){
     axios.get(`/auth/checkloggedIn`)
     .then((res) => {
-      // console.log(res, 'fE, log in************************')
       this.setState({ auth:true})
         //dispatch(setLoginSuccess(true));
     })
@@ -41,28 +44,39 @@ class App extends Component {
   }
 
   redirectTo(path){
+    console.log(path);
     this.setState({ redirect:path})
   }
   
   render(){
     const {redirect,shift,auth}=this.state; 
     const { isLoginSuccess }=this.props;
-    // console.log( isLoginSuccess, "this.props.isLoginSuccess**", redirect );
     if (!isLoginSuccess && !auth) {
       return (<div className="App">
                 <Login />
               </div>
       );
     }
+
     return (
       <div className="App">
         <SideNav toggleShift={this.toggleShift.bind(this)} redirectTo={ this.redirectTo.bind(this) }/>
         {
           redirect === 'HOME' ? (
-            <Main shift={ shift }/>
+            <Main shift={ shift } redirectTo={ this.redirectTo.bind(this)} />
           ) : redirect === 'SEARCH' ? (
-            <View shift={ shift }/>
-          ) : null
+            <View shift={ shift }/> 
+          ) : redirect === 'FAVORITES' ? (
+            <Favorites shift={ shift }/> 
+          ) :redirect === 'ALL_SONGS' ? (
+            <Library shift={ shift }/> 
+          ) :redirect === 'ARTISTS' ? (
+            <Artists shift={ shift } redirectTo={ this.redirectTo.bind(this)} />
+          ) : redirect === 'ARTIST_INFO' ? (
+            <ArtistInfo shift={ shift }/> 
+            )
+          :
+          null
         }
       </div>
     );
@@ -76,8 +90,6 @@ const mapStateToProps = (state) => {
     loginError: state.auth.loginError
   };
 }
-
-
 
 // const mapDispatchToProps = (dispatch) => {
 //   return {
