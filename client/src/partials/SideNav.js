e209dda3a8121450d867431a0bdfb40dad590031
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-26 02:11:47 -0500

    add redirect

--- client/src/partials/SideNav.js
+++ client/src/partials/SideNav.js
@@ -2,72 +2,85 @@ import React, { Component } from 'react';
 import PlaylistList from '../components/PlaylistList';
 import SearchBar from '../components/SearchBar';
 import PlayListMenu from '../components/PlayListMenu';
-import * as Ionicons from 'react-icons/lib/io'
+import * as Ionicons from 'react-icons/lib/md';
 import axios from 'axios';
 
- export default class SideNav extends Component {
-   constructor() {
-      super();
-      this.state = {
-          searchSong:null,
-          loadComponent:false,
-          apidataLoaded:false,
-          allPlayLists:null,
-          show:false,
-      }
+export default class SideNav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchSong: null,
+      loadComponent: false,
+      apidataLoaded: false,
+      allPlayLists: null,
+      show: false,
     }
-
-   componentWillMount(){
-     axios('/rPlayer/playlists', {method: 'GET'})
-     .then(res => {
-       console.log(res.data.data);
-       this.setState({
-         apidataLoaded:true,
-         allPlayLists:res.data.data
-       })
-     })
-     .catch(function (error) {
-        console.log(error);
-     });
   }
 
-   handleChange(e){
-    this.setState({searchSong: e.target.value})
-   };
+  componentWillMount() {
+  }
 
-    toggleShow(){
-      this.setState({show:!this.state.show})
-    }
-    
+  handleChange(e) {
+    this.props.redirectTo("SEARCH")
+  };
 
-    render() {
-      const {searchSong, loadComponent, playlist } = this.state;
-      console.log(playlist, 'playlist home');
-      console.log('search', this.state.searchSong )
-      return (
-             <nav role="navigation">
-                <div id="menuToggle">
-                  <input type="checkbox"  onClick={()=>this.props.toggleShift()}/>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <ul id="menu" style={{backgroundColor:'#493e42', display:'flex', flexDirection:'column', color:'#fff' }} >
-                   <SearchBar value={ this.state.searchSong } handleChange={this.handleChange.bind(this)  }  /*handleSubmit={ }*//>
-                    <a href="#" style={{ color: 'tomato'}}><li>Home</li></a>
-                    <a href="#" style={{ color: 'tomato'}}><li>Albums</li></a>
-                    <a href="#" style={{ color: 'tomato'}}><li>Artists</li></a>
-                    <a href="#" style={{ color: 'tomato'}}  onClick ={this.toggleShow.bind(this)} ><li>Playlists</li></a>
-                    <li style={{ display:this.state.show ? 'flex':'none', }}>
-                      <PlaylistList />
-                    </li>
-                    <li>
-                      <PlayListMenu />
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-      );
-    }
+  toggleShow() {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render() {
+    const { searchSong, loadComponent, playlist,show } = this.state;
+    console.log(playlist, 'playlist home');
+    console.log('search', this.state.searchSong)
+    console.log('show----', show)
+    return ( 
+      <nav role = "navigation" className={'menu-container'}>
+        <div id = "menuToggle" >
+          <input type = "checkbox" onClick = { () => this.props.toggleShift() }/> 
+          <span> </span> 
+          <span> </span> 
+          <span> </span> 
+          <ul className= "menu" style = {{ backgroundColor:'#F8F8F8', display: 'flex', flexDirection: 'column', color: '#fff'}}>
+             <li style={{marginTop:'-30px', textAlign:'left' , paddingBottom: '20px' }}>
+               <Ionicons.MdClose width={'30px'} 
+                  height={'2em'} 
+                  fill={'#ff7675'}
+              />
+            </li>
+            <li> 
+              <SearchBar 
+                value={ this.state.searchSong } 
+                handleChange={this.handleChange.bind(this)} 
+              />
+              </li>
+            <li>
+              <a href="#" style={{ /*color: 'tomato'*/}} onClick={()=>this.props.redirectTo("DEFAULT")}>Home</a>
+            </li>
+            <li>
+              <a href="#" style={{ /*color: 'tomato'*/}}>Albums</a>
+            </li>
+            <li>
+              <a href="#" style={{/*color: 'tomato'*/}} onClick={()=>this.props.redirectTo("ARTISTS")}>Artists</a>
+            </li>
+            <li>
+              <a href="#" style={{ /*color: 'tomato'*/}} onClick={()=>this.props.redirectTo("FAVORITES")}>Favorites</a>
+            </li>
+            <li>
+              <a href="#" style={{ /*color: 'tomato'*/}}  onClick ={this.toggleShow.bind(this)}>Playlists</a>
+            </li>
+            <li style={{ display:this.state.show ? 'flex':'none', }}>
+              <PlaylistList />
+            </li>
+            <li>
+              <PlayListMenu show={ this.state.show } redirectTo={ this.props.redirectTo} />
+            </li>
+          </ul>  
+        </div>
+      </nav>
+    );
+  }
 }
 
 
